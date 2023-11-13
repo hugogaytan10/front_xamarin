d2b9414d441b90b00f7bc0a2c1a2d59e23aae7f7
hugogaytan10 <estardus9@gmail.com>
2023-11-13 00:40:57 -0600

    avance

--- Parqueadero/Parqueadero/Parqueadero/Data/ApiService.cs
+++ Parqueadero/Parqueadero/Parqueadero/Data/ApiService.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Parqueadero.Models;
+using Xamarin.Essentials;
 
 namespace Parqueadero.Data
 {
@@ -16,19 +17,42 @@ namespace Parqueadero.Data
 		{
 			httpClient = new HttpClient();
 			// Puedes configurar aquí la URL base del API
-			 httpClient.BaseAddress = new Uri("http://192.168.0.6:45455/");
+			 httpClient.BaseAddress = new Uri("http://192.168.8.15:5150/");
 		}
 
 		public async Task<List<Vehiculo>> ObtenerVehiculos()
 		{
-			var response = await httpClient.GetAsync("api/vehiculos");
+            /*var response = await httpClient.GetAsync("api/vehiculos");
 			if (response.IsSuccessStatusCode)
 			{
 				var content = await response.Content.ReadAsStringAsync();
 				return JsonConvert.DeserializeObject<List<Vehiculo>>(content);
 			}
-			return null;
-		}
+			*/
+            string apiUrl = "http://192.168.8.15:45455/api/Vehiculos";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string content = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine(content);
+
+                        return JsonConvert.DeserializeObject<List<Vehiculo>>(content);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al obtener vehículos: {ex.Message}");
+            }
+
+            return null;
+        }
 
 		// Puedes agregar más métodos para realizar otras operaciones como agregar, actualizar, eliminar, etc.
 	}
